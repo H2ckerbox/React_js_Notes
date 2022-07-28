6b06f0af17e1c8969cdf6a458a50d8ea8fbd9fdf
H2ckerbox <h2ckerbox@gmail.com>
2022-07-29 03:35:36 +0530

    new notes

---
+++ code1.js
@@ -0,0 +1,13 @@
+function Welcome(prop){
+	return <h1>Hello,(prop.name)</h1>;
+}
+
+import React from 'react'
+
+// function Greet(){
+//     return <h1>hello manoj</h1>
+// }
+
+const Greet = () => <h1>Hello NEw</h1>
+
+export default Greet
---
+++ code2.js
@@ -0,0 +1,14 @@
+class welcome extends React.Component{
+	render(){
+		return <h1>Hello,{this.prop.name}</h1>
+	}}
+
+import React,{Component, component} from "react";
+
+class Welcome extends Component{
+    render(){
+        return <h1>Class component</h1>
+    }
+}
+
+export default Welcome;
